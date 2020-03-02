dbf377add8208e06e00dfe8bf6d7f12c0a00fe0b
dwlovelife <dwlovelife@126.com>
2020-03-02 23:52:54 +0800

    简单工厂模式 最初案例

---
+++ java-note/design-pattern/src/main/java/establish/factory/simplefactory/pizzastore/order/OrderPizza.java
@@ -0,0 +1,48 @@
+package establish.factory.simplefactory.pizzastore.order;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import establish.factory.simplefactory.pizzastore.pizza.CheesePizza;
+import establish.factory.simplefactory.pizzastore.pizza.GreekPizza;
+import establish.factory.simplefactory.pizzastore.pizza.Pizza;
+
+public class OrderPizza {
+	
+	//构造器
+	public OrderPizza(){
+		Pizza pizza = null;
+		String orderType; //订购Pizza的类型
+		do {
+			orderType = getType();
+			if(orderType.equals("greek")){
+				pizza = new GreekPizza();
+				pizza.setName("希腊Pizza");
+			} else if(orderType.equals("cheese")){
+				pizza = new CheesePizza();
+				pizza.setName("奶酪Pizza");
+			} else {
+				break;
+			}
+			//输出pizza 制作过程
+			pizza.prepare();
+			pizza.bake();
+			pizza.cut();
+			pizza.box();
+		} while(true);
+	}
+	
+	//写一个方法 可以获取客户端订购的披萨种类
+	private String getType(){
+		String str = "";
+		try {
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+			System.out.println("input pizza type");
+			str = bufferedReader.readLine();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return str;
+	}
+	
+}
---
+++ java-note/design-pattern/src/main/java/establish/factory/simplefactory/pizzastore/order/PizzaStore.java
@@ -0,0 +1,8 @@
+package establish.factory.simplefactory.pizzastore.order;
+
+//相当于一个客户端,发出订购
+public class PizzaStore {
+	public static void main(String[] args) {
+		new OrderPizza();
+	}
+}
