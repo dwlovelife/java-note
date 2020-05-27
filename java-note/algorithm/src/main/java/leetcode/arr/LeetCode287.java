939e015006df82251acd4a1f56f39f7f4d63781b
丁可乐 <dwlovelife@126.com>
2020-05-27 17:17:33 +0800

    leetCode287,560,974 前缀累加和问题

--- java-note/algorithm/src/main/java/leetcode/arr/LeetCode287.java
+++ java-note/algorithm/src/main/java/leetcode/arr/LeetCode287.java
@@ -6,7 +6,8 @@ import java.util.Map;
 /**
  * 寻找重复数
  * 给定一个包含 n + 1 个整数的数组 nums，其数字都在 1 到 n 之间（包括 1 和 n），
- * 可知至少存在一个重复的整数。假设只有一个重复的整数，找出这个重复的数。
+ * 可知至少存在一个重复的整数。
+ * 假设只有一个重复的整数，找出这个重复的数。
  */
 public class LeetCode287 {
     public static void main(String[] args) {
