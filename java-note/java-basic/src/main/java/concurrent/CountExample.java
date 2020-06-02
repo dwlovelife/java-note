84949278337ccaf9e9ee1b4db7dc4c1aa7a167f1
dwlovelife <dwlovelife@126.com>
2020-06-02 19:53:59 +0800

    配置新增

---
+++ java-note/java-basic/src/main/java/concurrent/CountExample.java
@@ -0,0 +1,37 @@
+package concurrent;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+public class CountExample {
+	private static int threadTotal = 200;
+	private static int clientTotal = 5000;
+
+	private static long count = 0;
+
+	public static void main(String[] args) {
+		ExecutorService exec = Executors.newCachedThreadPool();
+		final Semaphore semaphore = new Semaphore(threadTotal);
+		for (int index = 0; index < clientTotal; index++) {
+			exec.execute(() -> {
+				try {
+					semaphore.acquire();
+					semaphore.release();
+				} catch (Exception e) {
+					// TODO: handle exception
+				}
+			});
+		}
+		exec.shutdown();
+		log.info("count:{}",count);
+	}
+	
+	public static void add(){
+		count++;
+	}
+
+}
