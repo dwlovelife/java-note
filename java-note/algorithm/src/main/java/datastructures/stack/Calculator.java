f3da71aaa917b5c16dcf6af35fe08b4ab93bbf64
dwlovelife <dwlovelife@126.com>
2020-06-02 14:50:08 +0800

    计算器

--- java-note/algorithm/src/main/java/datastructures/stack/Calculator.java
+++ java-note/algorithm/src/main/java/datastructures/stack/Calculator.java
@@ -16,41 +16,63 @@ import java.util.Stack;
 public class Calculator {
     public static void main(String[] args) {
         //根据思路,完成表达式的运算
-        String expression = "3+2*6-2";
+        String expression = "13+2*6-20";
         //创建两个栈一个数字栈,一个符号栈
         Stack<Integer> numStack = new Stack<>();
-        Stack<Integer> operatorStack = new Stack<>();
+        Stack<Character> operatorStack = new Stack<>();
         //定义需要的相关变量
         char[] chars = expression.toCharArray();
         int num1 = 0;
         int num2 = 0;
         int operator = 0;
         int res = 0;
-        for (char ele : chars) {
+        StringBuilder nums = new StringBuilder();
+        for (int i = 0; i < chars.length; i++) {
             //判断元素是什么,然后进行相应的处理
+            char ele = chars[i];
             if (isOperator(ele)) {
                 //判断当前符号栈是否为空
                 if (!operatorStack.isEmpty()) {
                     //如果符号栈有操作符,就进行比较,如果当前的操作符的优先级小于等于栈中的操作符,就需要从数栈中pop出两个数
-                    num1 = numStack.pop();
-                    num2 = numStack.pop();
-                    operator = operatorStack.pop();
-                    res = cal(num1, num2, operator);
-                    //把运算的结果push数字栈
-                    numStack.push(res);
-                    //然后将
-                } else {
-
+                    //再从符号栈中pop出一个符号,进行运算,将得到的结果,入数栈,然后将当前的操作符入符号栈
+                    if (priority(ele) < priority(operatorStack.peek())) {
+                        num1 = numStack.pop();
+                        num2 = numStack.pop();
+                        operator = operatorStack.pop();
+                        res = cal(num1, num2, operator);
+                        //把运算的结果push数字栈
+                        numStack.push(res);
+                    }
                 }
-            } else {
-
+                operatorStack.push(ele);
+            } else { //如果是数则直接入数栈
+                /*
+                 * 分析思路
+                 * 1. 当处理多位数时,不能发现是一个数就立即入数栈,因为它可能是多位数
+                 * 2. 当处理数时,需要向expression的表达式index 后再看一位,如果是数则继续扫描,如果是符号则入栈
+                 * 3. 因此我们需要定义一个变量 字符串,用于拼接
+                 */
+                nums.append(ele);
+                if(i == chars.length - 1 || isOperator(chars[i + 1])){
+                    numStack.push(Integer.parseInt(nums.toString()));
+                    nums.delete(0,nums.length());
+                }
             }
         }
+        while (!operatorStack.isEmpty()) {
+            num1 = numStack.pop();
+            num2 = numStack.pop();
+            operator = operatorStack.pop();
+            res = cal(num2, num1, operator);
+            numStack.push(res);
+        }
+
+        System.out.printf("计算的结果:%d", numStack.pop());
     }
 
     //返回运算符的优先级,优先级是由程序员决定的,优先级使用数字表示
     //数字越大,则优先级越高
-    public static int priority(int operator) {
+    public static int priority(char operator) {
         if (operator == '*' || operator == '/') {
             return 1;
         } else if (operator == '+' || operator == '-') {
