1d7fa6b654e78bb019ac5cdad982400e373ae872
dingwei <dingwei@cheche365.com>
2020-06-03 13:58:05 +0800

    leetcode题解
    Signed-off-by: dingwei <dingwei@cheche365.com>

--- java-note/algorithm/src/main/java/leetcode/stack/LeetCode20.java
+++ java-note/algorithm/src/main/java/leetcode/stack/LeetCode20.java
@@ -5,6 +5,26 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Stack;
 
+/**
+ * 有效的括号
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ *
+ * 有效字符串需满足：
+ *
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * 注意空字符串可被认为是有效字符串。
+ * 输入: "()"
+ * 输出: tru
+ * 输入: "()[]{}"
+ * 输出: true
+ * 输入: "(]"
+ * 输出: false
+ * 输入: "([)]"
+ * 输出: false
+ * 输入: "{[]}"
+ * 输出: true
+ */
 public class LeetCode20 {
 
     static Map<Character,Character> match ;
