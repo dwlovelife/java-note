1b7c57868fd8bf74981d42bedd547d4ce7480fe6
dwlovelife <dwlovelife@126.com>
2020-06-03 11:41:41 +0800

    递归迷宫问题
    Signed-off-by: dingwei <dingwei@cheche365.com>

--- java-note/algorithm/src/main/java/datastructures/recursion/Maze.java
+++ java-note/algorithm/src/main/java/datastructures/recursion/Maze.java
@@ -25,7 +25,18 @@ public class Maze {
         //设置挡板,1表示
         map[3][1] = 1;
         map[3][2] = 1;
+        map[3][3] = 1;
+        map[3][4] = 1;
         //输出地图
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("----------------------------------");
+        setWay(map,1,1);
+
         for (int i = 0; i < map.length; i++) {
             for (int j = 0; j < map[i].length; j++) {
                 System.out.print(map[i][j] + " ");
@@ -42,6 +53,7 @@ public class Maze {
     //3. 如果小球能到map(6,5) 位置,说明通路找到.
     //4. 约定: 当map[i][j] 为0,表示该点没有走过 当为1的时候表示墙,2表示通路可以走,3表示该点已经走过,但是走不通
     //5. 在走迷宫时, 需要确定一个策略(方法) 下->右->上->左,如果该点走不通,再回溯
+
     /**
      * @param map 表示地图
      * @param i   i,j 表示从地图的哪个位置开始出发(1,1)
@@ -49,14 +61,27 @@ public class Maze {
      * @return 如果找到通路, 就返回true, 否则返回false
      */
     public static boolean setWay(int[][] map, int i, int j) {
-        if(map[6][5] == 2){
+        if (map[6][5] == 2) {
             return true;
-        }else{
-            if(map[i][j] == 0){
-                //TODO
+        } else {
+            if (map[i][j] == 0) { //如果该点还没有走过
+                //按照策略 下-> 右 -> 上 -> 左
+                map[i][j] = 2; //假定该点是可以走通
+                if (setWay(map, i + 1, j)) {
+                    return true;
+                } else if (setWay(map, i, j + 1)) {
+                    return true;
+                } else if (setWay(map, i - 1, j)) {
+                    return true;
+                } else if (setWay(map, i, j - 1)) {
+                    return true;
+                } else {
+                    map[i][j] = 3;
+                    return false;
+                }
             }
+            return false;
         }
-        return false;
     }
 
 }
