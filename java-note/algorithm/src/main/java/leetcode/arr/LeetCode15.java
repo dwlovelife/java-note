94c1700945260656a15b01be1f5a855477f94b0d
dingwei <dingwei@cheche365.com>
2020-06-05 10:53:50 +0800

    leetcode题解双指针
    Signed-off-by: dingwei <dingwei@cheche365.com>

---
+++ java-note/algorithm/src/main/java/leetcode/arr/LeetCode15.java
@@ -0,0 +1,60 @@
+package leetcode.arr;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 三数之和
+ * 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三元组。
+ */
+public class LeetCode15 {
+
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        List<List<Integer>> list = threeSum(nums);
+        System.out.println(list);
+    }
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        int len = nums.length;
+        if (len < 3) {
+            return list;
+        }
+        Arrays.sort(nums);
+        for (int i = 0; i < len; i++) {
+            int ele = nums[i];
+            if (ele > 0) {
+                break;
+            }
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int left = i + 1;
+            int right = len - 1;
+            while (left < right) {
+                if (left > i + 1 && nums[left] == nums[left - 1]) {
+                    left++;
+                    continue;
+                } else if (right < len - 1 && nums[right] == nums[right + 1]) {
+                    right--;
+                    continue;
+                }
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    list.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+                } else if (sum > 0) {
+                    right--;
+                } else {
+                    left++;
+                }
+            }
+        }
+        return list;
+    }
+
+}
