2ceced4e82ccec218ad5a7977ba95623dae2db50
dingwei <dingwei@cheche365.com>
2020-06-16 16:13:41 +0800

    数据结构-双向链表修改
    Signed-off-by: dingwei <dingwei@cheche365.com>

--- java-note/algorithm/src/main/java/datastructures/linkedlist/code/DoubleLinkedListDemo.java
+++ java-note/algorithm/src/main/java/datastructures/linkedlist/code/DoubleLinkedListDemo.java
@@ -1,111 +1,121 @@
 package datastructures.linkedlist.code;
 
+
 public class DoubleLinkedListDemo {
-	
-	
-	public static void main(String[] args) {
-		Nodes Nodes1 = new Nodes(2,"火神");
-		Nodes Nodes2 = new Nodes(1,"雷神");
-		Nodes Nodes3 = new Nodes(4,"花神");
-		Nodes Nodes4 = new Nodes(3,"水神");
-		
-//		addByOrder(Nodes1);
-//		addByOrder(Nodes2);
-//		addByOrder(Nodes3);
-//		addByOrder(Nodes4);
-		add(Nodes1);
-		add(Nodes2);
-		add(Nodes3);
-		add(Nodes4);
-		printAll();
-		
-	}
-	
-	static Nodes head = new Nodes(-1, null);
+    public static void main(String[] args) {
+		DoubleLinkedList<Integer> linkedList = new DoubleLinkedList<>();
+		linkedList.add(1);
+		linkedList.add(2);
+		linkedList.add(3);
+		System.out.println("size:" + linkedList.size());
+		linkedList.printAll();
+		System.out.println(linkedList.remove(1));
+		System.out.println(linkedList.remove(0));
+		System.out.println(linkedList.remove(0));
+    }
 
-	/**
-	 * 添加一个节点
-	 * 
-	 * @param Nodes
-	 */
-	public static void add(Nodes Nodes) {
-		Nodes temp = head;
+}
 
-		while (temp.next != null) {
-			temp = temp.next;
-		}
-		temp.next = Nodes;
-		Nodes.pre = temp;
-	}
+class DoubleLinkedList<E> {
+    private Node<E> head = new Node<>(null);
 
-	/**
-	 * 按顺序添加一个节点
-	 * 
-	 * @param Nodes
-	 */
-	public static void addByOrder(Nodes Nodes) {
-		if (Nodes.id <= -1) {
-			throw new IndexOutOfBoundsException("id越界了!");
-		}
-		Nodes temp = head;
-		while (temp.next != null) {
-			if (Nodes.id < temp.next.id) {
+    /**
+     * 添加一个元素
+     */
+    public void add(E element) {
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        Node<E> node = new Node<>(element);
+        temp.next = node;
+        node.pre = temp;
+		head.size++;
+    }
+
+    public E remove(int index){
+		checkIndexIsIllegal(index);
+		Node<E> temp = head;
+		for (int i = -1; temp.next != null; i++, temp = temp.next) {
+			if (index == i + 1) { //找到前一个元素
 				break;
 			}
-			temp = temp.next;
 		}
-
-		if (temp.next == null) {
-			temp.next = Nodes;
-			Nodes.pre = temp;
-		} else {
-			temp.next.pre = Nodes;
-			Nodes.next = temp.next;
-			Nodes.pre = temp;
-			temp.next = Nodes;
+		//因为已经前置了指针 所以removeNode必不等于空
+		Node<E> removeNode = temp.next;
+		if(removeNode.next == null){
+			temp.next = null;
+		}else{
+			temp.next = removeNode.next;
+			removeNode.next.pre = temp;
 		}
-
+		head.size--;
+		return removeNode.element;
 	}
-	
-	public static boolean update(int id,Nodes Nodes){
-		Nodes temp = head;
-		while(temp.next != null){
-			//if()
-			temp = temp.next;
+
+    /**
+     * 修改一个元素
+     */
+    public E update(int index, Node<E> node) {
+        checkIndexIsIllegal(index);
+        Node<E> temp = head.next;
+        for (int i = 0; temp.next != null; i++, temp = temp.next) {
+            if (index == i) {
+                break;
+            }
+        }
+        E oldVal = temp.element;
+        temp.element = node.element;
+        return oldVal;
+    }
+
+    public E get(int index){
+		checkIndexIsIllegal(index);
+		Node<E> temp = head.next;
+		for (int i = 0; temp.next != null; i++, temp = temp.next) {
+			if (index == i) {
+				break;
+			}
 		}
-		return true;
+		return temp.element;
 	}
-	
-	public static void printAll(){
-		Nodes temp = head;
-		System.out.println("-------打印之前-------");
-		while(temp.next != null){
-			temp = temp.next;
+
+	public int size(){
+    	return head.size;
+	}
+
+    public void printAll() {
+        Node temp = head.next;
+        System.out.println("-------打印之前-------");
+        while (temp != null) {
 			System.out.println(temp);
-		}
-		System.out.println("-------打印之后-------");
-	}
+			temp = temp.next;
+        }
+        System.out.println("-------打印之后-------");
+    }
 
-	
+    private void checkIndexIsIllegal(int index) {
+        if (index < 0 || index >= head.size) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    static class Node<E> {
+        int size;
+        E element;
+        Node<E> next;
+        Node<E> pre;
+
+
+        public Node(E element) {
+            this.element = element;
+        }
+
+        @Override
+        public String toString() {
+            return "Node [element=" + element + "]";
+        }
+    }
 }
 
-class Nodes {
-	int id;
-	String name;
-	Nodes next;
-	Nodes pre;
 
-	public Nodes(int id, String name) {
-		super();
-		this.id = id;
-		this.name = name;
-	}
-
-	@Override
-	public String toString() {
-		return "Nodes [id=" + id + ", name=" + name + "]";
-	}
-	
-	
-	
-}
