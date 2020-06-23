308e22e9abc59b77535a36a224a235175a8ad280
dwlovelife <dwlovelife@126.com>
2020-06-23 17:53:18 +0800

    二叉树的删除节点
    Signed-off-by: dingwei <dingwei@cheche365.com>

--- java-note/algorithm/src/main/java/datastructures/tree/BinaryTreeDemo.java
+++ java-note/algorithm/src/main/java/datastructures/tree/BinaryTreeDemo.java
@@ -30,9 +30,10 @@ public class BinaryTreeDemo {
 //        binaryTree.infixErgodic(); //中序遍历 2 1 5 3 4
 //        System.out.println("---后序遍历---");
 //        binaryTree.postErgodic(); //后序遍历 2 5 4 3 1
-
-        Node<Integer> node = binaryTree.infixSearch(5);
-        System.out.println(node);
+//        Node<Integer> node = binaryTree.infixSearch(5);
+////        System.out.println(node);
+        binaryTree.delNode(2);
+        binaryTree.preErgodic();
     }
 }
 
@@ -41,6 +42,18 @@ class BinaryTree<E> {
 
     public void setRoot(Node<E> root) {
         this.root = root;
+    }
+
+    public void delNode(E e) {
+        if (root != null) {
+            if (root.e.equals(e)) {
+                root = null;
+            } else {
+                root.delNode(e);
+            }
+        } else {
+            System.out.println("空数,不能删除");
+        }
     }
 
     /**
@@ -73,22 +86,22 @@ class BinaryTree<E> {
     /**
      * 前序查找
      */
-    public Node<E> preSearch(E e){
-        if(root != null){
+    public Node<E> preSearch(E e) {
+        if (root != null) {
             return root.preSearch(e);
         }
         return null;
     }
 
-    public Node<E> infixSearch(E e){
-        if(root != null){
+    public Node<E> infixSearch(E e) {
+        if (root != null) {
             return root.infixSearch(e);
         }
         return null;
     }
 
-    public Node<E> postSearch(E e){
-        if(root != null){
+    public Node<E> postSearch(E e) {
+        if (root != null) {
             return root.postSearch(e);
         }
         return null;
@@ -100,7 +113,7 @@ class BinaryTree<E> {
 class Node<E> {
     Node<E> left;
     Node<E> right;
-    private E e;
+    E e;
 
     Node(E e) {
         this.e = e;
@@ -147,6 +160,7 @@ class Node<E> {
 
     /**
      * 前序查找
+     *
      * @param e
      * @return
      */
@@ -176,52 +190,85 @@ class Node<E> {
 
     /**
      * 中序查找
+     *
      * @param e
      * @return
      */
-    public Node<E> infixSearch(E e){
+    public Node<E> infixSearch(E e) {
         Node<E> node = null;
-        if(this.left != null){
+        if (this.left != null) {
             node = this.left.infixSearch(e);
         }
-        if(node != null){
+        if (node != null) {
             return node;
         }
-        if(this.e.equals(e)){
+        if (this.e.equals(e)) {
             return this;
         }
-        if(this.right != null){
-            node  = this.right.infixSearch(e);
+        if (this.right != null) {
+            node = this.right.infixSearch(e);
         }
         return node;
     }
 
     /**
      * 后序查找
+     *
      * @param e
      * @return
      */
-    public Node<E> postSearch(E e){
+    public Node<E> postSearch(E e) {
         //判断当前结点的左子节点是否为空,如果不为空,则递归后序查找
         Node<E> node = null;
-        if(this.left != null){
+        if (this.left != null) {
             node = this.left.postSearch(e);
         }
-        if(node != null){ //说明在左子数找到
+        if (node != null) { //说明在左子数找到
             return node;
         }
 
         //如果左子树没有找到,则向右子树递归进行后序遍历查找
-        if(this.right != null){
+        if (this.right != null) {
             node = this.right.postSearch(e);
         }
-        if(node != null){
+        if (node != null) {
             return node;
         }
-        if(this.e.equals(e)){
+        if (this.e.equals(e)) {
             return this;
         }
         return node;
+    }
+
+    //递归删除节点
+    //1. 如果删除节点是叶子节点,则删除该节点
+    //2. 如果删除的节点是非叶子节点,则删除该子树
+    public void delNode(E e) {
+        //思路
+        /**
+         * 1. 因为我们的二叉树是单向的,所以我们是判断当前节点的子节点是否需要删除节点,而不能判断当前这个节点是不是需要删除的节点
+         * 2. 如果当前节点的左子节点不为空,并且左子节点 就是要删除节点 就将 this.left = null; 并且就返回(结束递归删除)
+         * 3. 如果当前节点的右子节点不为空,并且右子节点,就是要删除节点,就将 this.right = null;并且就返回(结束递归删除)
+         * 4. 如果第2步和第3步没有删除节点,那么我们就需要向左子树进行递归删除
+         * 5. 如果第4步也没有删除节点,则应当向右子树进行递归删除
+         */
+        //2. 如果当前节点的左子节点不为空,并且左子节点 就是要删除节点 就将 this.left = null; 并且就返回(结束递归删除)
+        if (this.left != null && this.left.e.equals(e)) {
+            this.left = null;
+            return;
+        }
+        //3. 如果当前节点的右子节点不为空,并且右子节点,就是要删除节点,就将 this.right = null;并且就返回(结束递归删除)
+        if (this.right != null && this.right.e.equals(e)) {
+            this.right = null;
+            return;
+        }
+        //如果第2步和第3步没有删除节点,那么我们就需要向左子树进行递归删除
+        if (this.left != null) {
+            this.left.delNode(e);
+        }
+        if (this.right != null) {
+            this.right.delNode(e);
+        }
     }
 
     @Override
