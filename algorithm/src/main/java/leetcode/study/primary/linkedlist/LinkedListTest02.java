cb40a09df11f88e339d0a53fcdfe776a67d85b5d
dwlovelife <dwlovelife@126.com>
2021-01-18 22:05:17 +0800

    LeetCode链表

--- algorithm/src/main/java/leetcode/study/primary/linkedlist/LinkedListTest02.java
+++ algorithm/src/main/java/leetcode/study/primary/linkedlist/LinkedListTest02.java
@@ -25,6 +25,21 @@ public class LinkedListTest02 {
         return rootNode.next;
     }
 
+    public static ListNode removeNthFromEnd2(ListNode head, int n) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode first = head;
+        ListNode second = dummy;
+        for(int i = 0; i < n; i++){
+            first = first.next;
+        }
+        while(first != null){
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        return dummy.next;
+    }
+
     public static int remove(ListNode head, int n){
         if(head.next == null){
             return n;
