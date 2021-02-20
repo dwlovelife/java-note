6512e868691f59b43c3bbdfcf06e42df902ad519
dwlovelife <dwlovelife@126.com>
2021-02-20 08:36:33 +0800

    算法

--- algorithm/src/main/java/datastructures/search/Kmp2.java
+++ algorithm/src/main/java/datastructures/search/Kmp2.java
@@ -1,8 +1,12 @@
 package datastructures.search;
 
+import java.util.Arrays;
+
 public class Kmp2 {
     public static void main(String[] args) {
-
+        String dest = "abababzabababa";
+        int[] result = kmpNext(dest);
+        System.out.println(Arrays.toString(result));
     }
 
     public static int[] kmpNext(String dest){
@@ -19,4 +23,20 @@ public class Kmp2 {
         }
         return next;
     }
+
+    public static int kmpSearch(String str1,String str2,int[] next){
+        for(int i = 0, j = 0; i < str1.length(); i++){
+            while(j > 0 && str1.charAt(i) != str1.charAt(i)){
+                j = next[j - 1];
+            }
+            if(str1.charAt(i) == str2.charAt(j)){
+                j++;
+            }
+            if(j == str2.length()){
+                return i - j + 1;
+            }
+        }
+        return -1;
+    }
+
 }
