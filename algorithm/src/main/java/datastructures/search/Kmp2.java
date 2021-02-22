20a992895c24c161b2c6278d28fb5fe43b3d7eb1
dwlovelife <dwlovelife@126.com>
2021-02-22 12:27:43 +0800

    kmp
    Signed-off-by: dingwei <dingwei@cheche365.com>

--- algorithm/src/main/java/datastructures/search/Kmp2.java
+++ algorithm/src/main/java/datastructures/search/Kmp2.java
@@ -31,6 +31,12 @@ public class Kmp2 {
 
     public static int kmpSearch(String str1, String str2, int[] next) {
         for (int i = 0, j = 0; i < str1.length(); i++) {
+            /**
+             *  假设主串与模式串已经存在部分匹配 那么i位置 发现两者不匹配 此时模式串需要查表回退
+             *  查看模式串不匹配的前一个位置 j - 1的 最大前缀（表里的数字代表 当前位置的最大公共前缀长度）
+             *  比如是4 abcd是最大前缀串(abcde...) 如果刚刚好 str2.charAt(j) = e  如果 刚好等于 str1.charAt(i)
+             *  能挂上 说明刚刚好 j并没有回退到初始位置 前缀串正好匹配
+             */
             while (j > 0 && str1.charAt(i) != str2.charAt(j)) {
                 j = next[j - 1];
             }
