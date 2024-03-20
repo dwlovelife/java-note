99759d894a37f460f7de5c1edfda3123d8db423a
dwlovelfe <dwlovelife@126.com>
2024-03-21 00:07:39 +0800

    面试

--- algorithm/src/main/java/datastructures/sort/QuickSort.java
+++ algorithm/src/main/java/datastructures/sort/QuickSort.java
@@ -3,48 +3,48 @@ package datastructures.sort;
 import java.util.Arrays;
 
 public class QuickSort {
-	public static void main(String[] args) {
-		int[] elements = { 1, 12, 2, 7, 9, 10, 4, 5, 10, 8 };
-		quickSort(elements, 0, elements.length - 1);
-		System.out.println(Arrays.toString(elements));
-	}
+    public static void main(String[] args) {
+        int[] elements = {5,1,2,3,4,6};
+        quickSort(elements, 0, elements.length - 1);
+        System.out.println(Arrays.toString(elements));
+    }
 
-	/**
-	 * 快速排序 从小到大排
-	 * 
-	 * @param arr
-	 * @param left
-	 * @param right
-	 * @return
-	 */
-	public static void quickSort(int[] arr, int left, int right) {
-		if (left > right) {
-			return;
-		}
-		int base = arr[left];
-		int l = left;
-		int r = right;
-		while (l < r) {
-			while (l < r && arr[r] >= base) {
-				r--;
-			}
-			while (l < r && arr[l] <= base) {
-				l++;
-			}
-			if (l < r) {
-				swap(arr, l, r);
-			}
-		}
-		swap(arr, l, left);
-		quickSort(arr, r + 1, right);
-		quickSort(arr, left, l - 1);
-	}
+    /**
+     * 快速排序 从小到大排
+     *
+     * @param arr
+     * @param left
+     * @param right
+     * @return
+     */
+    public static void quickSort(int[] arr, int left, int right) {
+        if (left > right) {
+            return;
+        }
+        int base = arr[left];
+        int l = left;
+        int r = right;
+        while (l < r) {
+            while (l < r && arr[r] >= base) {
+                r--;
+            }
+            while (l < r && arr[l] <= base) {
+                l++;
+            }
+            if (l < r) {
+                swap(arr, l, r);
+            }
+        }
+        swap(arr, left, l);
+        quickSort(arr, r + 1, right);
+        quickSort(arr, left, l - 1);
+    }
 
-	// swap方法
-	public static void swap(int[] nums, int left, int right) {
-		int temp = nums[left];
-		nums[left] = nums[right];
-		nums[right] = temp;
-	}
+    // swap方法
+    public static void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+    }
 
 }
--- java-basic/src/main/java/concurrent/threadpool/ThreadPoolTest01.java
+++ java-basic/src/main/java/concurrent/threadpool/ThreadPoolTest01.java
@@ -1,5 +1,7 @@
 package concurrent.threadpool;
 
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
 import java.util.concurrent.*;
 
 public class ThreadPoolTest01 {
@@ -7,6 +9,16 @@ public class ThreadPoolTest01 {
         ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
         ExecutorService fixedThreadPool = Executors.newFixedThreadPool(10);
         ExecutorService singleThreadPool = Executors.newSingleThreadExecutor();
-        ExecutorService executorService = new ThreadPoolExecutor(30, 30, 60, TimeUnit.SECONDS, new ArrayBlockingQueue<>(30));
+        ExecutorService executorService = new ThreadPoolExecutor(5, 40, 4, TimeUnit.SECONDS, new ArrayBlockingQueue<>(30));
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(40);
+        executor.setKeepAliveSeconds(4);
+        executor.setQueueCapacity(30);
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        executor.setThreadNamePrefix("service-thread-");
+        executor.initialize();
+
+        ThreadLocal
     }
 }
